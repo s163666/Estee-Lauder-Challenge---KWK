27d05b8364dc3defe73028763f251723dfab52fb
s163666 <72101330+s163666@users.noreply.github.com>
2023-07-31 16:27:37 -0500

    Initial Commit

---
+++ esteeLauder/esteeLauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  esteeLauderApp.swift
+//  esteeLauder
+//
+//  Created by Anahita Kaur on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct esteeLauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
